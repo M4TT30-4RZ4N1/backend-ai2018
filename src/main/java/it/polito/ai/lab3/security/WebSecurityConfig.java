523b6804bf9a6f308bd1ac70da198040f7ee270e
Raffaele <raffysommy@gmail.com>
2018-06-13 21:33:55 +0200

    Corso tentatviiii3234444423424dsadsafasfasfsa

--- src/main/java/it/polito/ai/lab3/security/WebSecurityConfig.java
+++ src/main/java/it/polito/ai/lab3/security/WebSecurityConfig.java
@@ -55,19 +55,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers("/tokens/**").permitAll()
 		.anyRequest().authenticated();
     }
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
 
 
 }
